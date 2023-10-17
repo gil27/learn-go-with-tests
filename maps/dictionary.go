516ceeaa157a89a74b8b458cabfaa3ded04d115e
Gil Gomes <gilgomesp@gmail.com>
2023-10-17 19:47:12 -0300

    finish maps

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,14 +1,21 @@
 package main
 
-import "errors"
+const (
+  ErrNotFound = DictionaryErr("could not find the word you were looking for")
+  ErrWordExists = DictionaryErr("cannot add word because it already exists")
+  ErrWordDoesNotExist = DictionaryErr("cannot update word because it does not exist")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+  return string(e)
+}
 
 type Dictionary map[string]string
 
-var ErrNotFound = errors.New("could not find the word you were looking for")
-
 func (d Dictionary) Search(word string) (string, error) {
   definition, ok := d[word]
-
   if !ok {
     return "", ErrNotFound
   }
@@ -16,8 +23,36 @@ func (d Dictionary) Search(word string) (string, error) {
   return definition, nil
 }
 
-func (d Dictionary) Add(word, definition string) {
-  d[word] = definition
+func (d Dictionary) Add(word, definition string) error {
+  _, err := d.Search(word)
+  switch err {
+  case ErrNotFound:
+    d[word] = definition
+  case nil:
+    return ErrWordExists
+  default:
+    return err
+
+  }
+
+  return nil
 }
 
-// parei aqui: https://quii.gitbook.io/learn-go-with-tests/go-fundamentals/maps#write-the-test-first-3
+func (d Dictionary) Update(word, definition string) error {
+  _, err := d.Search(word)
+  switch err {
+  case ErrNotFound:
+    return ErrWordDoesNotExist
+  case nil:
+    d[word] = definition
+  default:
+    return err
+
+  }
+
+  return nil
+}
+
+func (d Dictionary) Delete(word string) {
+  delete(d, word)
+}
