1524957b100ee0a3acbeaafa158d6ed7185d8bab
Gil Gomes <gilgomesp@gmail.com>
2023-10-24 10:03:39 -0300

    mocking

--- mocking/mocking.go
+++ mocking/mocking.go
@@ -1,27 +1,31 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"os"
-	"time"
+  "fmt"
+  "io"
+  "os"
+  "time"
 )
+
+type Sleeper interface {
+  Sleep()
+}
+
+type ConfigurableSleeper struct {
+  duration time.Duration
+  sleep    func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+  c.sleep(c.duration)
+}
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-type Sleeper interface {
-	Sleep()
-}
-
-type DefaultSleeper struct{}
-
-func (d *DefaultSleeper) Sleep() {
-  time.Sleep(1 * time.Second)
-}
-
 func Countdown(out io.Writer, sleeper Sleeper) {
-  for i := countdownStart; i > 0; i -- {
+
+  for i := countdownStart; i > 0; i-- {
     fmt.Fprintln(out, i)
     sleeper.Sleep()
   }
@@ -30,6 +34,6 @@ func Countdown(out io.Writer, sleeper Sleeper) {
 }
 
 func main() {
-  sleeper := &DefaultSleeper{}
+  sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
   Countdown(os.Stdout, sleeper)
 }
